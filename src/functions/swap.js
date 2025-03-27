8d650e26c84fd11379d3db55efa3f0f7145f124f
balliende <bernardita.alliende@uc.cl>
2025-03-27 19:31:34 -0300

    Swap changed

--- src/functions/swap.js
+++ src/functions/swap.js
@@ -1,10 +1,4 @@
 export function swap(data, n, m) {
-    return data.map(row => {
-        const newRow = [...row];
-        const temporal = newRow[n];
-        newRow[n] = newRow[m];
-        newRow[m] = temporal;
-        return newRow;
-    })
+    return data.map((array) =>[n,m].reduce((a, b) => ([array[a], array[b]] = [array[b], array[a]], array)));
 }
 
